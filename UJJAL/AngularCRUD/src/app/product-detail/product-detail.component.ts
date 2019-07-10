804d76657f6229760bd97126ead5dbba8012a400
arindom38 <arindomkundu18@gmail.com>
2019-07-10 15:47:30 +0600

    modified product-details

--- UJJAL/AngularCRUD/src/app/product-detail/product-detail.component.ts
+++ UJJAL/AngularCRUD/src/app/product-detail/product-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { Product } from '../product';
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  product: Product = { id: null, prod_name: '', prod_desc: '', prod_price: null, updated_at: null };
+  
+  constructor(private route: ActivatedRoute, private api: ApiService, private router: Router) { }
 
   ngOnInit() {
+    this.getProductDetails(this.route.snapshot.params['id']);
   }
 
+  getProductDetails(id) {
+    this.api.getProduct(id)
+      .subscribe(data => {
+        this.product = data;
+        console.log(this.product);
+       
+      });
+
+  }
+
+  deleteProduct(id) {
+    
+    this.api.deleteProduct(id)
+      .subscribe(res => {
+         
+          this.router.navigate(['/products']);
+        }, (err) => {
+          console.log(err);
+         
+        }
+      );
+  }
 }
