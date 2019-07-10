138270e716664db3c32800f2df24589af64d748b
ashraf1410499 <abuashraf501@gmail.com>
2019-07-10 21:08:56 +0600

    crud done

--- ASHRAF/CRUDOperationWithWebApi/src/app/modules/shared/employee.service.ts
+++ ASHRAF/CRUDOperationWithWebApi/src/app/modules/shared/employee.service.ts
@@ -14,14 +14,29 @@ export class EmployeeService {
  
   Url = "http://localhost:3000/Employee";
 
+  currentEmployee: Employee = {
+        id: null,
+      fullname : '',
+        email : '',
+        department : '',
+        contact : null,
+        address : ''
+  }
+
   constructor(private httpcall: HttpClient) { }
 
   getAllEmployees(): Observable<Employee[]>{
     return this.httpcall.get<Employee[]>(this.Url, headerOption);
   }
 
-  deleteThisEmployee(id: number): Observable<Employee[]>{
-     return this.httpcall.delete<Employee[]>(this.Url + '/' + id, headerOption);
+  deleteThisEmployee(id: number): Observable<Employee>{
+     return this.httpcall.delete<Employee>(this.Url + '/' + id, headerOption);
   }
 
+  createEmployee(emp : Employee): Observable<Employee> {
+    return this.httpcall.post<Employee>(this.Url, emp , headerOption);
+  }
+  updateEmployee(emp : Employee): Observable<Employee> {
+    return this.httpcall.put<Employee>(this.Url + '/' + emp.id, emp , headerOption);
+  }
 }
