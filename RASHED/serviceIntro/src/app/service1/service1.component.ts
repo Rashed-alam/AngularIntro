cf5d7a68a580289ecc73ad1c6e52498cfeba1b9a
rashed <rashed1123alam@gmail.com>
2019-07-11 16:04:54 +0600

    .

--- RASHED/serviceIntro/src/app/service1/service1.component.ts
+++ RASHED/serviceIntro/src/app/service1/service1.component.ts
@@ -1,6 +1,17 @@
 import { CUser } from './../user';
 import { SummationService } from './../summation.service';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import{Observable} from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Authorization': 'my-auth-token'
+  })
+};
 
 
 @Component({
@@ -10,9 +21,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Service1Component implements OnInit {
 
-public users =[];
+public users:CUser[] =[];
 
-  constructor(private list:SummationService) {
+  constructor(private list:SummationService, private http:HttpClient) {
   
    }
 
@@ -26,16 +37,17 @@ public users =[];
       .subscribe(data => this.users = data
        );
     }
+    create(userform:NgForm){
+      console.log(userform);
+      this.http.post('http://localhost:3000/user',userform, httpOptions).subscribe(data=>{
+        console.log(data)
+      })
     
-    add(vauser:CUser){
-      console.log(vauser);
-    
-      this.list
-        .add(vauser)
-        .subscribe(data => this.users.push(data));
+      // this.list
+      //   .add(userform)
+      //   .subscribe();
      
-
-    };
+};
   
 
 
