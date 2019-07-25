473bac337bd5fcc0514f5ae21585fd15ddfd7224
moumita saha <moumita023@gmail.com>
2019-07-24 17:21:48 -0700

    saving data using array

--- Moumita/Moumita/src/app/new/new.component.ts
+++ Moumita/Moumita/src/app/new/new.component.ts
@@ -1,28 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { DepFlags } from '@angular/compiler/src/core';
+import { typeSourceSpan } from '@angular/compiler';
 
 @Component({
   selector: 'app-new',
   templateUrl: './new.component.html',
-  styleUrls: ['./new.component.css']
+styleUrls: ['./new.component.css']
+
+  
+
+
+  
 })
 export class NewComponent implements OnInit {
- 
-  constructor() { }
+
+  demoList:any=[]
+  constructor() {}
 
   ngOnInit() {
    
   }
+
+  
+  
+
   submitInfo(loginForm: NgForm) {
-   console.log("ok");
+   //console.log("ok");
    const name = loginForm.controls["name"].value;
    const email = loginForm.controls["email"].value;
    const pass = loginForm.controls["pass"].value;
-  //  const password = loginForm.controls["pass"].value;
-  console.log(name);
+
+ // this.demoList.push(name,email,pass);
+  this.demoList.push({
+    name:name,
+    email:email,
+    pass:pass
+
+  })
+  console.log(this.demoList)
+   console.log(name);
    console.log(email);
    console.log(pass);
-  //  console.log(password);
+ 
   
   }
 }
