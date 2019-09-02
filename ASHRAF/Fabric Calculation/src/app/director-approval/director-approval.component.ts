b7b7c2d9baf6e953bd4b8a3e7038c47e1ffcdba0
abuashraf73 <32354150+abuashraf73@users.noreply.github.com>
2019-09-02 10:56:44 +0600

    fabric calculation folder added to my folders

---
+++ ASHRAF/Fabric Calculation/src/app/director-approval/director-approval.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-director-approval',
+  templateUrl: './director-approval.component.html',
+  styleUrls: ['./director-approval.component.css']
+})
+export class DirectorApprovalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
