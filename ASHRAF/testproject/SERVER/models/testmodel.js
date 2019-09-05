8996a14403882680cac6c7ceee1e339949278676
abuashraf73 <32354150+abuashraf73@users.noreply.github.com>
2019-09-05 17:25:44 +0600

    api testing

--- ASHRAF/testproject/SERVER/models/testmodel.js
+++ ASHRAF/testproject/SERVER/models/testmodel.js
@@ -1,16 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
-const UserSchema = new mongoose.Schema({
+const UserSchema = new Schema({
     firstName: String,
     lastName: String,
-    info: [
-        {
-            phone: String,
-            address: String
-        }
-    ]
+    info: [{
+        phone: String,
+        address: String
+    }]
 });
+
+
 
 module.exports = mongoose.model('user', UserSchema);
--- ASHRAF/testproject/SERVER/routes/userApi.js
+++ ASHRAF/testproject/SERVER/routes/userApi.js
@@ -8,53 +8,52 @@ router.get('/all',(req,res,next)=>{
         res.send(a);
     }).catch(next);
 });
+router.get('/get/single', function(req, res){
+    // get by single stylecode
+    // UserSchema.aggregate([
+    //     { $unwind :'$info'},
+    //     { $match : {'info.phone': "1" }},
+    
+    //     ]).then(data => {
+    //         res.send(data)
+    //     })
 
 
-//POSTING THE new User input INTO THE DB
-// router.post('/new',(req,res,next)=>{ 
-//     UserSchema.create(req.body).then(function(a){
-//         res.send(a);
-//     }).catch(next);
-   
-// });
+//remove data from array by stylecode
+UserSchema.update({_id : '5d70d86212b3d3289c57081a'}, {$pull : {  info : {phone: '1'}}});
 
-// router.post('/insertAnother',(req,res,next)=>{ 
-//     UserSchema.findOne({firstName:'Abu'}).then(function(a){
-//        a.info.push({phone:'12345',address:'malibagh'});
-//        a.save().then(function(a){
-//         res.send(a);
-//        })
-//     }).catch(next);
-   
-// });
+})
+
+
 //POST ER JONNO 
 router.post('/insert/:firstName',(req,res,next)=>{ 
     UserSchema.findOne({firstName:req.params.firstName}).then(function(a){
         if(a == null){
             UserSchema.create(req.body).then(function(a){
-                res.send("if part a ase akhon");
+                res.send("New User");
             }).catch(next);
         }
         else{
             a.info.push({phone:req.body.phone,address:req.body.address});
             a.save().then(function(a){
-             res.send("else part a ase akhon");
+             res.send("Old User");
          }).catch(next);
 }
 });
 });
 
   //for deleting entries
-  router.delete('/delete/:firstName/:phone',(req,res,next)=>{
-    UserSchema.findOne({firstName:req.params.firstName}).then(function(a){
-      a.info.pull({phone: req.params.phone});
-      a.update({phone: req.params.phone});
-    //   res.send(a);
-    // UserSchema.deleteOne({a:req.params.phone}).then(function(){
-    //     console.log(a);
-    //    res.send(a);  
-    // }).catch(next);
-    });
-  });
+router.delete('/delete/:firstName/:phone',(req,res,next)=>{
+    UserSchema.findOne({firstName: req.params.firstName}).then(function(a){
+        console.log(a.info);
+      // UserSchema.update({_id : '5d70cd353ce91035504e1e87'}, {$pull : {  info : {phone: '1'}}});
+        UserSchema.findOneAndDelete([{phone:req.params.phone}]).then(function(x){
+         console.log(x);
+            // UserSchema.deleteOne(x).then(function(b){
+            //     res.send("deleted");
+            // })
+        })
+    }).catch(next);
+});
 
 module.exports = router; 
