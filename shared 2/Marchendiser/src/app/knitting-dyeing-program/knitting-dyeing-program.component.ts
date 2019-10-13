feb3d0ea06d3e7cd4e6dee1d942850d381fba8b8
rashed <rashed1123alam@gmail.com>
2019-10-13 13:30:57 +0600

    .

--- shared 2/Marchendiser/src/app/knitting-dyeing-program/knitting-dyeing-program.component.ts
+++ shared 2/Marchendiser/src/app/knitting-dyeing-program/knitting-dyeing-program.component.ts
@@ -1,6 +1,7 @@
 import { knittingNdyeing } from './../models/knittingNdyeing';
 import { knittingTypeModel } from './../models/knittingType.model';
 import { KnittingNDyeingService } from './../services/knitting-ndyeing.service';
+import { FabricPriceServiceService } from 'src/app/services/fabric-price-service.service';
 import { Component, OnInit } from '@angular/core';
 
 
@@ -10,10 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./knitting-dyeing-program.component.css']
 })
 export class KnittingDyeingProgramComponent implements OnInit {
+  AllReference: any[];
+  fabricReport: any = [];
+  buyerinfo: any;
   color: any = [];
   knittingType: any = [];
+  AllinfoData: any = [];
   arr: any[][] = [];
+  styleCode: any = [];
+  ref: any;
+  stl: any;
+  sum = 0;
+  bName: any;
   knit = { kintting: [], referenceId: '', styleCode: ' ' };
+  ttl = [];
+  ttlCol = [];
   knittData: knittingNdyeing = {
     referenceId: '',
     styleCode: '',
@@ -24,18 +36,78 @@ export class KnittingDyeingProgramComponent implements OnInit {
     }]
   }
 
-  constructor(public KD: KnittingNDyeingService) { }
+  constructor(public KD: KnittingNDyeingService, public Fc: FabricPriceServiceService) { }
   ngOnInit() {
     for (let i = 0; i < 1000; i++) {
       this.arr[i] = [];
       for (let j = 0; j < 1000; j++) {
-        // console.log(this.arr[i][j]);
         this.arr[i][j] = 0;
         // console.log(this.arr[i][j]);
       }
     }
+    this.getAllReference();
+    this.getAllBuyerInfo();
+    this.getAlldata();
 
   }
+
+  getAllReference() {
+    this.Fc.getAllref()
+      .subscribe(
+        (data) => {
+          this.AllReference = data;
+          // console.log(this.AllReference);
+
+        });
+
+  }
+  getreportdata(a) {
+    this.ref = " ";
+    this.ref = a;
+    console.log(this.ref);
+    this.Fc.getAllData(a).subscribe(
+      (data) => {
+        this.fabricReport = data;
+        this.bName = this.fabricReport[0].buyerName;
+        // this.styleCode=this.fabricReport[0].styleCode;
+        //  console.log(this.fabricReport);
+        for (let i = 0; i < this.fabricReport.length; i++) {
+          this.styleCode[i] = (this.fabricReport[i].styleCode);
+          // console.log(this.styleCode[i]);
+        }
+        //  console.log(this.styleCode);
+      }
+    );
+
+    //console.log(this.styleCode);
+
+  }
+  getAllBuyerInfo() {
+    this.Fc.getAllBuyer().
+      subscribe((data) => {
+        this.buyerinfo = data;
+        //  console.log(this.buyerinfo);
+
+      });
+  }
+  getbuyerInfo(k) {
+    this.AllReference = [];
+    for (let i = 0; i < this.AllinfoData.length; i++) {
+      if (this.AllinfoData[i].buyerCode === k) {
+        this.AllReference.push(this.AllinfoData[i].referenceId);
+      }
+
+    }
+
+  }
+  getAlldata() {
+    this.Fc.getallData()
+      .subscribe(
+        (data) => {
+          this.AllinfoData = data;
+        });
+  }
+
   addColor(m) {
     this.color.push(m);
   }
@@ -43,9 +115,11 @@ export class KnittingDyeingProgramComponent implements OnInit {
     this.color = this.color.filter(h => h !== n);
   }
   addKnittingType(l) {
-    console.log(l);
+    //  console.log(l);
     this.knittingType.push(l);
-    console.log(this.knittingType);
+
+ //  this.knittingType = ['k', 'b', 'c', 'ttl'];
+    // console.log(this.knittingType);
 
   }
   deleteKnittingType(m) {
@@ -56,51 +130,54 @@ export class KnittingDyeingProgramComponent implements OnInit {
 
   getvalue(f: any, i: number, j: number) {
 
-    
+
     this.arr[i][j] = f;
   }
 
-
+  getstl(k) {
+    this.stl = " ";
+    this.stl = k;
+    console.log(this.stl);
+  }
 
   showArray(arr) {
-    this.knit.referenceId = 'test-1';
-    this.knit.styleCode = 'est-1';
+    this.ttl=[];
+    this.ttlCol=[];
+
+    this.knit.referenceId = this.ref;
+    this.knit.styleCode = this.stl;
     for (let k = 0; k < this.color.length; k++) {
+      let m=0;
+      this.sum=0;
+      let r = 0;
       for (let l = 0; l < this.knittingType.length; l++) {
-        //  // console.log('no:' + this.color[k], ' ', this.knittingType[l], ' ', arr[k][l]);
-        //   this.knit.kintting.knittingType = this.knittingType[l];
-        //   this.knit.kintting.color = this.color[k];
-        //   //  console.log(arr[k][l]);
-        //   //  let j = arr[k][l];
-        //   this.knit.kintting[0].weight = arr[k][l];
+       r = r + parseInt(arr[k][l]);
+        m = m+ parseInt (arr[l][k]);
+        console.log(arr[k][l]);
         this.knit.kintting.push({
           knittingType: this.knittingType[l],
           color: this.color[k],
-          weight:arr[k][l]
+          weight: arr[k][l]
         })
 
       }
+      this.ttlCol.push(m);
+      this.ttl.push(r);
+      console.log(this.ttl);
+      console.log(this.ttlCol);
 
 
     }
-    this.KD.getdata(this.knit).subscribe(res=>{
-          });
-        
+    this.KD.getdata(this.knit).subscribe(res => {
+    });
+
     console.log(this.knit);
 
   }
   clear() {
-    for (let i = 0; i < 1000; i++) {
-      this.arr[i] = [];
-      for (let j = 0; j < 1000; j++) {
-        // console.log(this.arr[i][j]);
-        this.arr[i][j] = 0;
-        // console.log(this.arr[i][j]);
-      }
-    }
-
     this.color = [];
     this.knittingType = [];
+    this.knit.kintting = [];
   }
 }
 
