bd819bd908369c4b9e1f8e8504afeb3ea7387a3d
abuashraf73 <32354150+abuashraf73@users.noreply.github.com>
2019-10-14 12:41:45 +0600

    API

--- Shared/Merchandizer/server/routes/cuttingApi.js
+++ Shared/Merchandizer/server/routes/cuttingApi.js
@@ -47,10 +47,116 @@ router.get('/everything', (req, res, next) => {
     }).catch(next);
 });
 
+//GET ALL entries INFORMATION  by reference id and styleCode
+router.post('/all/:referenceId/:styleCode', (req, res, next) => {
+    CuttingSchema.findOne({
+        referenceId: req.params.referenceId,
+        styleCode: req.params.styleCode
+    }).then(function(a) {
+        if (a != null) {
+            var alldata = JSON.parse(JSON.stringify(a.cutting));
+            var ref = a.referenceId;
+            var styC = a.styleCode;
+            var remark = a.remarks;
+            for (i = 0; i < alldata.length; i++) {
+                alldata[i].referenceId = ref;
+                alldata[i].styleCode = styC;
+                alldata[i].remarks = remark;
+            }
+            res.send(alldata);
+        } else {
+            var x = [];
+            res.send(x);
+        }
+    }).catch(next);
+});
+
+//EDIT by matching reference id and _id
+router.put('/update/:referenceId/:_id', function(req, res, next) {
+    CuttingSchema.findOne({ referenceId: req.params.referenceId }).then(function(a) {
+        // res.send(a);
+        CuttingSchema.update({
+                "referenceId": req.params.referenceId,
+                "cutting._id": req.params._id
+
+            }, {
+                $set: {
+                    "remarks": req.body.remarks,
+                    "cutting.$.color": req.body.cutting[0].color,
+                    "cutting.$.size": req.body.cutting[0].size,
+                    "cutting.$.weight": req.body.cutting[0].weight
+                }
+            },
+            function(err, result) {
+                if (err) {
+                    console.log('ERROR: ' + err);
+                    res.send({ 'error': 'An error has occurred' });
+                } else {
+                    res.send(result);
+                }
+            });
+    });
+});
+//EDIT item by their nested Id
+router.put('/update/:_id', function(req, res, next) {
+    CuttingSchema.findOne({ _id: req.params._id }).then(function(a) {
+        // res.send(a);
+        CuttingSchema.update({
+                "cutting._id": req.params._id
+            }, {
+                $set: {
+                    "remarks": req.body.remarks,
+                    "cutting.$.color": req.body.cutting[0].color,
+                    "cutting.$.size": req.body.cutting[0].size,
+                    "cutting.$.weight": req.body.cutting[0].weight
+                }
+            },
+            function(err, result) {
+                if (err) {
+                    console.log('ERROR: ' + err);
+                    res.send({ 'error': 'An error has occurred' });
+                } else {
+                    res.send(result);
+                }
+            });
+    });
+});
 
 
+//DELETE nested items
+router.delete('/delete/:referenceId/:styleCode/:_id', function(req, res, next) {
+    CuttingSchema.findOne({ referenceId: req.params.referenceId, styleCode: req.params.styleCode }).then(function(a) {
+        var lengthofA = a.cutting.length;
+        if (lengthofA > 1) {
+            var array = a.cutting;
+            var l = a.cutting.length;
 
+            for (let i = 0; i < array.length; i++) {
+                //  console.log('check='+ array[i].styleCode);
+                if (a.cutting[i]._id == req.params._id) {
+                    console.log('ok')
+                    array.splice(i, 1);
+                }
+            }
+            a.cutting = array;
+            a.save().then(function(a) {
+                res.send({ "message": "ENTRY SUCCESSFULLY DELETED 1" });
+            }).catch(next);
+        } else {
+            CuttingSchema.findOneAndDelete({ referenceId: req.params.referenceId }).then(function(fabricentry) {
+                res.send({ "message": "ENTRY SUCCESSFULLY DELETED 2" });
+            }).catch(next);
+        }
+    });
 
+});
 
+//DELETE WHOLE OBJECT
+router.delete('/delete/:_id', (req, res, next) => {
+    CuttingSchema.findOneAndDelete({ _id: req.params._id }).then(function(fabricentry) {
+        res.send({ "message": "Deleted Sucessfully" });
+    }).catch(next);
+
+});
 
 module.exports = router;
