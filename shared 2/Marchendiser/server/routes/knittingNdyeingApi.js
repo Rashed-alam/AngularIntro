8838a71bdb79439ee57df9d38cef77f7b8b968f8
rashed <rashed1123alam@gmail.com>
2019-10-17 10:14:31 +0600

    .v

--- shared 2/Marchendiser/server/routes/knittingNdyeingApi.js
+++ shared 2/Marchendiser/server/routes/knittingNdyeingApi.js
@@ -49,5 +49,46 @@ router.post('/all/:referenceId/:styleCode', (req, res, next) => {
       // console.log(a.kintting);
     }).catch(next);
 });
+// get and delete then create
+router.post('/update/:referenceId/:styleCode', (req, res, next) => {
+    console.log(req.body)
+    //,styleCode: req.params.styleCode
+    knittingNdyeingType.findOne({ 'referenceId': req.params.referenceId,'styleCode': req.params.styleCode }).then(function (a) {
+        if (a == null) {
+            knittingNdyeingType.create(req.body).then(function (a) {
+                console.log(a);
+                res.send(a);
+
+            }).catch(next);
+       } 
+        else {
+            knittingNdyeingType.update(
+                { referenceId: req.params.referenceId,
+                    styleCode: req.params.styleCode
+                },
+
+                {
+                    $addToSet: {
+                        kintting: [{
+                            knittingType: req.body.kintting[0].knittingType,
+                            color: req.body.kintting[0].color,
+                            weight: req.body.kintting[0].weight,
+                            row: req.body.kintting[0].row,
+                            col: req.body.kintting[0].col,
+                        }],
+                    }
+                }, { multi: true },
+                function (err, result) {
+                    if (err) {
+                        //  console.log('ERROR: ' + err);
+                        res.send({ 'error': 'An error has occurred' });
+                    } else {
+                        res.send(result);
+                    }
+                })
+
+        }
+    });
+});
 
 module.exports=router;
