5f3523a3b53e037e6408ca86c6bc41059b0703ea
Rashed Alam <rashed1123alam@gmail.com>
2019-10-20 14:52:59 +0600

    edit in command line

--- shared 2/Marchendiser/server/routes/knittingNdyeingApi.js
+++ shared 2/Marchendiser/server/routes/knittingNdyeingApi.js
@@ -1,65 +1,95 @@
 const express = require('express');
 const router = express.Router();
-const knittingNdyeingType=require('../models/knittingNdyeing');
+const knittingNdyeingType = require('../models/knittingNdyeing');
 var Promise = require('promise');
 router.post('/new/:referenceId/:styleCode', (req, res, next) => {
-    console.log(req.body)
-    //,styleCode: req.params.styleCode
-    knittingNdyeingType.findOne({ 'referenceId': req.params.referenceId,'styleCode': req.params.styleCode }).then(function (a) {
-        if (a == null) {
-            knittingNdyeingType.create(req.body).then(function (a) {
-                console.log(a);
-                res.send(a);
+  knittingNdyeingType.findOne({
+    'referenceId': req.params.referenceId,
+    'styleCode': req.params.styleCode
+  }).then(function (a) {
+    if (a == null) {
+      knittingNdyeingType.create(req.body).then(function (a) {
+        res.send(a);
 
-            }).catch(next);
-       } 
-        else {
-            knittingNdyeingType.update(
-                { referenceId: req.params.referenceId,
-                    styleCode: req.params.styleCode
-                },
+      }).catch(next);
 
-                {
-                    $addToSet: {
-                        kintting: [{
-                            knittingType: req.body.kintting[0].knittingType,
-                            color: req.body.kintting[0].color,
-                            weight: req.body.kintting[0].weight,
-                            row: req.body.kintting[0].row,
-                            col: req.body.kintting[0].col,
-                        }],
-                    }
-                }, { multi: true },
-                function (err, result) {
-                    if (err) {
-                        //  console.log('ERROR: ' + err);
-                        res.send({ 'error': 'An error has occurred' });
-                    } else {
-                        res.send(result);
-                    }
-                })
+    } else {
+      res.send(a);
+      knittingNdyeingType.update({
+          referenceId: req.params.referenceId,
+          styleCode: req.params.styleCode
+        },
 
-        }
-    });
+        {
+          $addToSet: {
+            kintting: [{
+              knittingType: req.body.kintting[0].knittingType,
+              color: req.body.kintting[0].color,
+              weight: req.body.kintting[0].weight,
+              row: req.body.kintting[0].row,
+              col: req.body.kintting[0].col,
+            }],
+          }
+        }, {
+          multi: true
+        },
+        function (err, result) {
+          if (err) {
+            //  console.log('ERROR: ' + err);
+            res.send({
+              'error': 'An error has occurred'
+            });
+          } else {
+            res.send(result);
+          }
+        })
+
+    }
+  });
 });
 //get every object
 router.post('/all/:referenceId/:styleCode', (req, res, next) => {
-    knittingNdyeingType.findOne({ 'referenceId': req.params.referenceId,'styleCode': req.params.styleCode }).then(function (a) {
-        res.send(a);
-      // console.log(a.kintting);
-    }).catch(next);
+  knittingNdyeingType.findOne({
+    'referenceId': req.params.referenceId,
+    'styleCode': req.params.styleCode
+  }).then(function (a) {
+    res.send(a);
+    // console.log(a.kintting);
+  }).catch(next);
 });
 // get and delete then creates
 
 router.post('/update/:referenceId/:styleCode', (req, res, next) => {
-    knittingNdyeingType.findOne({ 'referenceId': req.params.referenceId,'styleCode': req.params.styleCode }).then(function (_err,a) {
-            knittingNdyeingType.findOneAndDelete({a}).then(
-                knittingNdyeingType.create(req.body).then(function (a) {
-                    console.log(a);
-                    res.send(a);
-                }).catch(next)
-            )   
-    });
+  knittingNdyeingType.findOne({
+    'referenceId': req.params.referenceId,
+    'styleCode': req.params.styleCode
+  }).then(function (a) {
+    if (a == null) {
+
+      res.send("empty");
+    } else {
+      knittingNdyeingType.findOneAndDelete({
+        'referenceId': req.params.referenceId,
+        'styleCode': req.params.styleCode
+      }).then(
+        knittingNdyeingType.create(req.body).then(function (a) {
+          console.log(a);
+          res.send(a);
+        }).catch(next)
+      )
+      //  res.send(a);
+    }
+  });
 });
 
-module.exports=router;
+// delete api
+router.delete('/delete/:referenceId/:styleCode', (req, res, next) => {
+  knittingNdyeingType.findOneAndDelete({
+    'referenceId': req.params.referenceId,
+    'styleCode': req.params.styleCode
+  }).then(function (a) {
+    res.send(re)
+  });
+});
+
+module.exports = router;
